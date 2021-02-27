b67cc26323ae3f3372d553fc2b22b0dafd1ae70f
Brett Haines <bhaines3@gatech.edu>
2021-02-26 20:33:47 -0500

    Modify code to fit Rust style guidelines

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,10 @@ async fn main() {
     let start_time = SystemTime::now();
 
     // Set up default options
-    let mut quiet_mode : bool = false;
-    let mut timeout : u64 = 10;
-    let mut target_addrs : Vec<String> = Vec::new();
-    let mut port_list : Vec<u16> = (0..u16::MAX).map(|x| x+1).collect();
+    let mut quiet_mode: bool = false;
+    let mut timeout: u64 = 10;
+    let mut target_addrs: Vec<String> = Vec::new();
+    let mut port_list: Vec<u16> = (0..u16::MAX).map(|x| x+1).collect();
 
     // Parse user args
     let args: Vec<String> = std::env::args().collect();
@@ -35,7 +35,10 @@ async fn main() {
             "-q" | "--quiet"   => quiet_mode = true,
             "-t" | "--timeout" => timeout = args[index + 1].parse::<u64>().unwrap(),
             "-b" | "--batch"   => println!("-b is not supported yet!"),
-            "-d" | "--debug"   => SimpleLogger::new().with_level(log::LevelFilter::Info).init().unwrap(),
+            "-d" | "--debug"   => SimpleLogger::new()
+                                    .with_level(log::LevelFilter::Info)
+                                    .init()
+                                    .unwrap(),
             _ => (),
         }
     }
@@ -49,7 +52,8 @@ async fn main() {
     }
 
     // Run through list of scan_addr calls in the FuturesUnordered
-    while let Some(_) = futures.next().await { }
+    while let Some(_) = futures.next().await { 
+    }
 
     if !quiet_mode {
         println!("All done -- {} seconds elapsed", start_time.elapsed().unwrap().as_secs());
@@ -58,16 +62,16 @@ async fn main() {
 
 
 // Read a list of addresses from a file
-fn parse_addr_list(filename : &String, addr_list : &mut Vec<String>) {
+fn parse_addr_list(fname: &String, addr_list: &mut Vec<String>) {
 
-    log::info!("Reading addresses from file at {}", filename);
+    log::info!("Reading addresses from file at {}", fname);
 
     // Try to open the file for reading
-    let file = match OpenOptions::new().read(true).open(filename) {
+    let file = match OpenOptions::new().read(true).open(fname) {
         Ok(file) => file,
         Err(e) => match e.kind() {
-            ErrorKind::NotFound => panic!("Could not find file at {}", filename),
-            ErrorKind::PermissionDenied => panic!("You don't have permission to read {}", filename),
+            ErrorKind::NotFound => panic!("Could not find file at {}", fname),
+            ErrorKind::PermissionDenied => panic!("You don't have permission to read {}", fname),
             _ => panic!("An unknown file error occurred"),
         },
     };
@@ -81,7 +85,7 @@ fn parse_addr_list(filename : &String, addr_list : &mut Vec<String>) {
 
 
 // Create a list of ports, either from a comma-separated list or a range
-fn parse_port_list(port_string : &str) -> Vec<u16> {
+fn parse_port_list(port_string: &str) -> Vec<u16> {
 
     log::info!("Parsing port list {}", port_string);
 
@@ -94,7 +98,10 @@ fn parse_port_list(port_string : &str) -> Vec<u16> {
     // Range -- match to two groups of 1-5 digits separated by two '.' chars or a '-'
     if Regex::new(r"^\d{1,5}(\.\.|-)\d{1,5}$").unwrap().is_match(port_string) {
         let delim = Regex::new(r"\.\.|-").unwrap();
-        let bounds : Vec<u16> = delim.split(port_string).map(|x| x.parse::<u16>().unwrap()).collect();
+        let bounds : Vec<u16> = delim
+                                .split(port_string)
+                                .map(|x| x.parse::<u16>().unwrap())
+                                .collect();
         return (bounds[0]..bounds[1]).collect::<Vec<u16>>();
     }
 
@@ -105,7 +112,7 @@ fn parse_port_list(port_string : &str) -> Vec<u16> {
 
 
 // Scan a given list of ports on a given address
-async fn scan_addr(address : String, port : &u16, timeout_value : u64) {
+async fn scan_addr(address: String, port: &u16, timeout_value: u64) {
 
     log::info!("Starting scan on {} port {}", address, port);
 
@@ -114,6 +121,8 @@ async fn scan_addr(address : String, port : &u16, timeout_value : u64) {
         if let Ok(_) = timeout(
             Duration::from_secs(timeout_value),
             stream
-        ).await { println!("{}:{}", address, port); }
+        ).await {
+            println!("{}:{}", address, port);
+        }
     }.await;
 }
